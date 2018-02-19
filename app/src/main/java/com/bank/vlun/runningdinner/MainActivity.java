6936fc5e3348136fb1bd6a403096f59d4c2e5501
Gümüssu, Metin <metin8893@gmail.com>
2018-02-19 14:43:35 +0100

    In der Actionbar (Toolbar) Settings Menü hinzugefügt Hier wird später der User sein Profilbild und seine Einstellungen sehen und verändern können.
    Für die Settings habe ich das Template dass von Google bereitgestellt wird benutzt.
    Ein Link für ein Tutorial habe ich in der SettingsActivity Klasse als Kommentar hinterlegt.
    
    Die test.xml wird benötigt (sollte umbenannt werden) um den SettingsScreen so zu verändern, dass das Profilbild mit angezeigt wird. Ansonsten bestehen SettingsActivities nur aus Listen.
     Das Profillbild sollte klein und Rund sein, das hat leider nicht ganz geklappt..

--- app/src/main/java/com/bank/vlun/runningdinner/MainActivity.java
+++ app/src/main/java/com/bank/vlun/runningdinner/MainActivity.java
@@ -3,8 +3,13 @@ package com.bank.vlun.runningdinner;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar main_toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
 
         btn_Start = findViewById(R.id.btn_Start);
 
@@ -26,5 +33,40 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    // Diese Methode holt sich die main_menu.xml und zeigt sie in der appbar an
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    /**
+     * Was soll passieren wenn der Button in der Appbar geklickt wurde
+     * @param item Das Item, dass in der main_menu.xml definiert wurde
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_profilesettings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this, "Appbar Button", Toast.LENGTH_SHORT).show();
+
+                // Ruft die Settingsactivity auf
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
